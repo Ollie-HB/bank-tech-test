ec6d4c9c2081de8a600d3ba304d3beee432b2470
Ollie Beck <ollieb94@hotmail.co.uk>
2022-11-17 10:23:16 +0000

    newline added to join and taken out of the transaction string

--- spec/statement.test.js
+++ spec/statement.test.js
@@ -20,7 +20,7 @@ describe('Statement', () => {
 
     it('returns a statement showing one deposit', () => {
       account.deposit(100)
-      expect(statement.displayTransactions()).toEqual('date || credit || debit || balance\n27/05/2023 || £100.00 || || £100.00\n');
+      expect(statement.displayTransactions()).toEqual('date || credit || debit || balance\n27/05/2023 || £100.00 || || £100.00');
     });
 
     // currently only works if the oldest transaction is entered first + unknown plus symbols and '\n' after new lines in Node + line 34 is far too long
@@ -29,7 +29,7 @@ describe('Statement', () => {
       account.deposit(2000, '13/01/2023')
       account.withdraw(500, '14/01/2023')
       expect(statement.displayTransactions())
-      .toEqual('date || credit || debit || balance\n14/01/2023 || || £500.00 || £2500.00\n13/01/2023 || £2000.00 || || £3000.00\n10/01/2023 || £1000.00 || || £1000.00\n');
+      .toEqual('date || credit || debit || balance\n14/01/2023 || || £500.00 || £2500.00\n13/01/2023 || £2000.00 || || £3000.00\n10/01/2023 || £1000.00 || || £1000.00');
     })
 });
 
--- src/statement.js
+++ src/statement.js
@@ -8,14 +8,14 @@ class Statement {
     let transactionsArray = this.account.getTransactions()
     let formattedStatementLines = transactionsArray.map(transaction => {
         if (transaction['amount'] > 0) {    // deposit
-           return (`${transaction['transactionDate']} || £${transaction['amount'].toFixed(2)} || || £${transaction['remainingBalance'].toFixed(2)}\n`)
+           return (`${transaction['transactionDate']} || £${transaction['amount'].toFixed(2)} || || £${transaction['remainingBalance'].toFixed(2)}`)
         } else {      //withdrawal
           // multiplying withdrawal 'amount' by -1 to change it from a negative number into a positive number (for the statement output)
-          return (`${transaction['transactionDate']} || || £${(transaction['amount'] * -1).toFixed(2)} || £${transaction['remainingBalance'].toFixed(2)}\n`)
+          return (`${transaction['transactionDate']} || || £${(transaction['amount'] * -1).toFixed(2)} || £${transaction['remainingBalance'].toFixed(2)}`)
         }
       });
       // statement rows only print in order if the oldest transaction is entered first
-      return formattedStatementLines.reverse().join('');
+      return formattedStatementLines.reverse().join('\n');
   }
 
   displayTransactions() {
